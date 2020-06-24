10c8c0a291580fccde69e7a19b3e718a58ca9a2d
Yida Da <carriedada@gmail.com>
2020-06-23 21:34:16 -0700

    works, right?

---
+++ Poller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Poller
+//
+//  Created by Yida Zhang on 2020-06-23.
+//  Copyright © 2020 Yida Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Poller/ViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ViewModel.swift
+//  Poller
+//
+//  Created by Yida Zhang on 2020-06-23.
+//  Copyright © 2020 Yida Zhang. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class ViewModel {
+    
+    static var publicContainer = CKContainer.default().publicCloudDatabase
+    
+}
