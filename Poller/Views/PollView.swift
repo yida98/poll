f8651c5f5eea7dc5f3c51fa7e355bd2857577322
Yida Da <carriedada@gmail.com>
2020-12-24 22:54:04 -0800

    months of crashing b/c one missed period before shadow.

--- Poller/Views/PollView.swift
+++ Poller/Views/PollView.swift
@@ -16,17 +16,18 @@ struct PollView: View {
         VStack {
             Text(poll.title) // Title
                 .foregroundColor(.orange)
-            VStack {
+            VStack{
                 ForEach(poll.pollItems.indices, id: \.self) { itemIndex in
                     Text(String(itemIndex))
                         .padding(.horizontal, 20)
                         .multilineTextAlignment(.center)
                         .frame(width: Constant.pollItemSize.width, height: Constant.pollItemSize.height)
                         .foregroundColor(Color.gray)
-                        shadow(radius: 4)
                         .overlay(RoundedRectangle(cornerRadius: Constant.pollItemSize.height/2)
                             .stroke()
                             .frame(width: Constant.pollItemSize.width, height: Constant.pollItemSize.height)
+                            .shadow(radius: 4)
+
                         )
                 }
             }
